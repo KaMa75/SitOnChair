4907e37ca536fc6ba29e49760f2b6888fb458c57
KaMa75 <karolmarszalek@o2.pl>
2019-01-03 11:52:31 +0100

    Dodanie automatycznej zmiany slajdow

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,8 @@ var nextBtn = document.querySelector('.navigation.next-btn');
 var dots = document.querySelectorAll('.slider .dot');
 var cnt = 0;
 var maxCnt = 2;
+var autoSlide = setTimeout(nextSlide, 2000);
+var slideTime = 6000;
 
 function findSlides() {
     return document.querySelectorAll('.slides .slide');
@@ -14,18 +16,26 @@ function removeCenter2left(event) {
     event.target.removeEventListener('animationend', removeCenter2left);
 }
 
+function addListeners() {
+    nextBtn.addEventListener('click', nextSlide);
+    prevBtn.addEventListener('click', previousSlide);
+    autoSlide = setTimeout(nextSlide, slideTime);
+}
+
+function removeListeners() {
+    prevBtn.removeEventListener('click', previousSlide);
+    nextBtn.removeEventListener('click', nextSlide);
+    clearTimeout(autoSlide);
+}
+
 function removeRight2center(event) {
     event.target.classList.remove('right2center');
     event.target.removeEventListener('animationend', removeRight2center);
-    nextBtn.addEventListener('click', nextSlide);
-    prevBtn.addEventListener('click', previousSlide);
 }
 
 function removeCenter2right(event) {
     event.target.classList.remove('center2right');
     event.target.removeEventListener('animationend', removeCenter2left);
-    prevBtn.addEventListener('click', previousSlide);
-    nextBtn.addEventListener('click', nextSlide);
 }
 
 function removeLeftt2center(event) {
@@ -55,9 +65,8 @@ function removeAnimations(slides){
 }
 
 function nextSlide(event) {
+    removeListeners();
     var slides = findSlides();
-    nextBtn.removeEventListener('click', nextSlide);
-    prevBtn.removeEventListener('click', previousSlide);
     removeAnimations(slides);
     slides[1].classList.add('center2left');
     slides[2].classList.add('right2center');
@@ -71,13 +80,13 @@ function nextSlide(event) {
         cnt=0;
     }
     addClass(cnt);
+    addListeners();
 }
 
 function previousSlide(event) {
+    removeListeners();
     var slides = findSlides();
-    prevBtn.removeEventListener('click', previousSlide);
-    nextBtn.removeEventListener('click', nextSlide);
-    removeAnimations(slides);    
+    removeAnimations(slides);
     slides[0].classList.add('left2center');
     slides[1].classList.add('center2right');
     slides[0].addEventListener('animationend', removeLeftt2center);
@@ -90,7 +99,7 @@ function previousSlide(event) {
         cnt=maxCnt;
     }
     addClass(cnt);
+    addListeners();
 }
 
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', previousSlide);
+addListeners ();
